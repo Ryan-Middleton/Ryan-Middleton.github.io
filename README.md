4d09f92cdd095ba0c39612462e5a3d3ee65b870a
Ryan-Middleton <ryanmiddleton04@gmail.com>
2023-12-30 22:14:17 -0500

    first draft. Tabs, information, formating

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Ryan-Middleton.github.io
 Personal Portfolio Website
+
+Built using HTML and CSS
+
+Hosted on Github Pages
+
+WIP
---
+++ websiteFunctions.js
@@ -0,0 +1,21 @@
+function openTab(evt, tabName){
+
+    //Define variables
+    var x, content, links;
+    content = document.getElementsByClassName("content");
+    links = document.getElementsByClassName("links");
+
+    //Hide all content
+    for(x = 0; x < content.length; x++) { 
+        content[x].style.display = "none";
+    }
+    
+    //Remove active
+    for(x = 0; x < links.length; x++){
+        links[x].className = links[x].className.replace(" active","");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.tabName += " active";
+    
+}
