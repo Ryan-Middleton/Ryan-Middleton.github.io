f124be25ccc114a6cb6fa93ba1baea9430b1b36d
Ryan-Middleton <ryanmiddleton04@gmail.com>
2024-05-08 18:16:00 -0400

    testing chatGPT

--- websiteFunctions.js
+++ websiteFunctions.js
@@ -1,3 +1,25 @@
+function openTab(evt, tabName) {
+    // Hide all tab content
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    // Remove 'active' class from all tab links
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    
+    // Show the specific tab content
+    document.getElementById(tabName).style.display = "block";
+    
+    // Add 'active' class to the clicked tab link
+    evt.currentTarget.classList.add("active");
+}
+
+/*
 function openTab(evt, tabName){
 
     //Define variables
