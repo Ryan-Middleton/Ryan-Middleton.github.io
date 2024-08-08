b0e51a63645064688ea2bad3380f794fb188c297
Ryan-Middleton <ryanmiddleton04@gmail.com>
2024-08-07 22:34:05 -0400

    Revert back

--- websiteFunctions.js
+++ websiteFunctions.js
@@ -23,22 +23,39 @@ function openTab(evt, tabName){
     
 }
 
+function openTab(evt, tabName){
+
+    //Define variables
+    var x, content, links;
+    content = document.getElementsByClassName("content");
+    links = document.getElementsByClassName("links");
+
+    //Hide all content
+    for(x = 0; x < content.length; x++) { 
+        content[x].style.display = "none";
+    }
+    
+    //Remove active
+    for(x = 0; x < links.length; x++){
+        links[x].className = links[x].className.replace(" active","");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.tabName += " active";
+    
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
-    const announcer = document.querySelector('.announcer');
+    //const announcer = document.querySelector('.announcer');
 
     let board = ['','',''
                 ,'','',''
-                ,'','',''];
-    let currentPlayer = 'X';
+                ,'','','']
+    let currentPlayer = 'X'
     let isGameActive = true;
-    
-
-    const X_WON = 'X_WON';
-    const O_WON = 'O_WON';
-    const TIE = 'TIE';
 
     const wins = [
         [0, 1, 2],
@@ -49,9 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
         [2, 5, 8],
         [0, 4, 8],
         [6, 4, 2],
-    ];
-
-    
+    ]
 
     function handleResultValidation(){
         let roundWon = false;
@@ -65,37 +80,18 @@ window.addEventListener('DOMContentLoaded', () => {
             }
             if(a === b && b === c){
                 roundWon = true;
-                break;
+                return;
             }
         }
 
         if(roundWon){
-            announce(currentPlayer === 'X' ? X_WON : O_WON);
             isGameActive = false;
             return;
         }
-
-        if(!board.includes('')){
-            announce(TIE);
-        }
     }
 
-    const announce = (type) => {
-        switch(type){
-            case O_WON:
-                announcer.innerHTML = 'Player O Won';
-                break;
-            case X_WON:
-                announcer.innerHTML = 'Player X Won';
-                break;
-            case TIE:
-                announcer.innerText = 'Tie';
-        }
-        announcer.clasList.remove('hide');
-    };
-
     if(!board.includes('')){
-        resetBoard();
+        resetBoard
     }
 
     const updateBoard = (index) => {
@@ -110,7 +106,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     tiles.forEach( ( tile, index) => {
         tile.addEventListener('click', () => userAction(tile,index));
-    });
+    })
 
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive){
@@ -123,18 +119,17 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     const changePlayer = () => {
-        playerDisplay.classList.remove('player${currentPlayer}');
+        playerDisplay.classList.remove('player${currentPlayer}')
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        playerDisplay.innerText = currentPlayer;
+        playerDisplay.innerText = currentPlayer
         playerDisplay.classList.add('player${currentPlayer}')
     }
 
     const resetBoard = () => {
         board = ['','',''
                 ,'','',''
-                ,'','',''];
+                ,'','','']
         isGameActive = true;
-        //announcer.classList.add('hide');
 
         if(currentPlayer === 'O'){
             changePlayer();
